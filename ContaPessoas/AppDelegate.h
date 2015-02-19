2a2d32def6f73a6aff789f95800ccd9b70d8bc8e
Patricia Machado de Abreu <Patricia@MacBook-Pro-de-Patricia.local>
2015-02-19 14:23:13 -0200

    Comentario

--- ContaPessoas/AppDelegate.h
+++ ContaPessoas/AppDelegate.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Vinicius Miana. All rights reserved.
 //
 
+
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- ContaPessoas/Contador.m
+++ ContaPessoas/Contador.m
@@ -5,6 +5,7 @@
 //  Created by Vinicius Miana on 2/18/15.
 //  Copyright (c) 2015 Vinicius Miana. All rights reserved.
 //
+//Patricia Abreu
 
 #import <Foundation/Foundation.h>
 #import "Contador.h"
